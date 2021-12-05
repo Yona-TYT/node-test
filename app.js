10b691a97677bea39162e051c6ca473fbecf1a18
Yona-TYT <yonatan.el.amigo@gmail.com>
2021-12-05 13:30:03 -0400

    Secciones de inicio desplegables, organizacion de servicios por categoria

--- app.js
+++ app.js
@@ -24,6 +24,8 @@ app.set('view engine', 'pug');
 //var router = express.Router();
 
 //Variables para la pagina ---------------------------------------------------------
+
+//Informacion general----------------------------------------------------------------------------------------------------------------------
 var src_dom = "https://www.hidroingelec.cl";
 var src_desc = "Servicios de mantenimiento e instalación de equipos hidráulicos.";
 var src_motto = "Ingeniería hidráulica y eléctrica.";
@@ -32,37 +34,118 @@ var src_info = "Servicios y asesorías Hidroingelec S.P.A. Somos una empresa ded
 var src_vision = "Ser reconocidos por los valores y profesionalidad anticipando el futuro del área hídrica, a través de tecnología de vanguardia y el desarrollo del personal.";
 var src_mision = "Ser líderes en asesoría y prestación de servicios en la captación, y abastecimiento de agua, proporcionar servicios integrales, soluciones flexibles a través de innovación tecnológica y gente comprometida, para así superar las expectativas de nuestros clientes.";
 var src_address = "Calle fray Juan de ibarguen # 249 diagonal al hogar Alemán kolbe Alto comuna Osorno Los Lagos";
+// -------------------------------------------------------------------------------------------------------------------------------------------------
 
+//Menus , enlaces  etc ----------------------------------------------------------------------------
 var src_contact =[
 				{font: "fa fa-envelope-o", inner : "correo@mail.com", url : "mailto:correo@mail.com", title : "Correo Electronico", text : "Dirección de correo electrónico."},
 				{font: "fa fa-phone", inner : "001001000", url : "tel:001001000", title : "Telefono", text : "Linea telefónica para atención al cliente."},
 				{font: "fa fa-whatsapp", inner : "+58 414000000", url : "https://wa.me/58414000000", title : "WhatsApp", text : "WhatsApp oficial de la empresa."}
-			]
+			];
 var src_social =[
 					{title : "Facebook", font : "fab fa-facebook-f", url : "/", text : "Siguenos en Facebook"}, 
 					{title : "Twitter", font : "fab fa-twitter", url : "/", text : "Siguenos en Twitter"}, 
 					{title : "Instagram", font : "fab fa-instagram", url : "/", text : "Siguenos en Instagram"}
-				]
+				];
 var src_links =	[
 				{name : "Inicio", url : "/"}, 
 				{name : "Contacto", url : "contacto"},
 				{name : "Servicios", url : "servicios"},
 				{name : "Galeria", url : "galeria"}
-			]
-var src_autor = {name : "Yona-TYT", url : "https://forum.simutrans.com/index.php?action=profile;u=8727"}
+			];
+var src_autor = {name : "Yona-TYT", url : "https://forum.simutrans.com/index.php?action=profile;u=8727"};
 var src_lang =	[
 				{name : "es", inner : "Español", url : "index.html"}, {name : "en", inner : "Ingles", url : "index.html"}
-			]
+			];
 
-var src_serv =	[
-					{ sud : "servicios", id : "bombas", title : "Bombas", text : "Mantenimiento correctivo y preventivo en salas de bombas.", type : "Mantenimiento"},
-					{ sud : "servicios", id : "estanques", title : "Estanques", text : "Venta e instalación de estanques de presión.", type : "instalación"},
-					{ sud : "servicios", id : "piscinas", title : "Piscinas", text : "Limpieza y mantenimiento a piscinas.", type : "Mantenimiento"},
-					{ sud : "servicios", id : "tableros", title : "Tableros", text : "Diseño y control en tableros de fuerza y control.", type : "Diseño"},
-					{ sud : "servicios", id : "plantas", title : "Plantas", text : "Construcción de plantas de tratamiento.", type : "Diseño"},
-					{ sud : "servicios", id : "terreno", title : "Técnicos", text : "Servicio Técnico en terreno.", type : "General"}
-				]
+//-----------------------------------------------------------------------------------------------------
+
+
+//Servicios --------------------------------------------------------------------------------------------
+var cat_list = {vis : "visita", agu : "agua", equ : "equipos", man : "mantenimiento", ele : "electrico"};
+var all_serv =	[
+					{
+					  swiper : true, category : cat_list.equ, id : "bombas", title : "Bombas",
+					  text : "Mantenimiento correctivo y preventivo en salas de bombas.", type : "Mantenimiento"
+					},
+					{ 
+					  swiper : true, category : cat_list.agu, id : "estanques", title : "Estanques", 
+				 	  text : "Venta e instalación de estanques de presión.", type : "instalación"
+					},
+					{ 
+					  swiper : true, category : cat_list.man, id : "piscinas", title : "Piscinas",
+					  text : "Limpieza y mantenimiento a piscinas.", type : "Mantenimiento"
+					},
+					{ 
+					  swiper : true, category : cat_list.ele, id : "tableros", title : "Tableros",
+					  text : "Diseño y control en tableros de fuerza y control.", type : "Diseño"
+					},
+					{
+					  swiper : true, category : cat_list.agu, id : "plantas", title : "Plantas",
+					  text : "Construcción de plantas de tratamiento.", type : "Diseño"
+					},
+					{
+					  swiper : true, category : cat_list.vis, id : "terreno", title : "Técnicos",
+					  text : "Servicio Técnico en terreno.", type : "General"
+					}
+				];
+
+var src_serv = [];
+
+//Servicios por categoria
+var elm_siz = 5; //cat_list.length;
+var src_list_elem = new Array(elm_siz);
+
+//Se inicializan los array bidimencionales
+for(var j = 0; j<elm_siz;j++){
+	src_list_elem[j] = new Array();
+	//console.log(""+j);
+}
+
+
+//Se crea la lista para los servicios en swiper y las categorias añ inicio
+all_serv.forEach(function(serv) {
+	if(serv.swiper) {
+		src_serv.push(
+						{ 
+							id : serv.id, title : serv.title, text : serv.text, type : serv.type
+						}
+					);
+	}
+	if(serv.category == cat_list.vis){
+		src_list_elem[0].push({text : serv.text, id : serv.id});
+		//console.log(serv.text);
+	}
+	else if(serv.category == cat_list.agu){
+		src_list_elem[1].push({text : serv.text, id : serv.id});
+		//console.log(serv.text);
+	}
+	else if(serv.category == cat_list.equ){
+		src_list_elem[2].push({text : serv.text, id : serv.id});
+		//console.log(serv.text);
+	}
+	else if(serv.category == cat_list.man){
+		src_list_elem[3].push({text : serv.text, id : serv.id});
+		//console.log(serv.text);
+	}
+	else if(serv.category == cat_list.ele){
+		src_list_elem[4].push({text : serv.text, id : serv.id});
+		//console.log(serv.text);
+	}
+  //console.log(serv.text);
+});
 //----------------------------------------------------------------------------------
+
+// PAgina de inicio -----------------------------------------------------------------
+var elm_a = ["yonatanss","Igggggggg"];
+var elm_b = ["yonatanss","Igggggggg"];
+var elm_c = ["yonatanss","Igggggggg"];
+var elm_d = ["yonatanss","Igggggggg"];
+var elm_e = ["yonatanss","Igggggggg"];
+var elm_f = ["yonatanss","Igggggggg"];
+
+// -----------------------------------------------------------------------------------
+
 
 
 
@@ -72,7 +155,7 @@ app.get('/', function (req, res) {
 			domain : src_dom, desc : src_desc, motto : src_motto, title : src_title,
 			contact : src_contact, links : src_links, autor : src_autor, lang : src_lang,
 			social : src_social, address : src_address, address : src_address, info : src_info,
-			mision : src_mision, vision : src_vision
+			mision : src_mision, vision : src_vision, list_elem : src_list_elem
 		}
 	); 
 });
@@ -80,7 +163,7 @@ app.get('/', function (req, res) {
 app.get('/swiper', function (req, res) {
 	res.render('swiper', 
 		{
-			serv : src_serv
+			serv : src_serv, links : src_links
 		}
 	); 
 });
